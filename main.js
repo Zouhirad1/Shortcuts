cbfe7c4d5c1e3f9f4a28f70115cb9cc5567aeb55
Zouhirad1 <168578500+Zouhirad1@users.noreply.github.com>
2026-05-21 12:10:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow, shell } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+
+  win.loadFile('shortcuts.html')
+
+  // Open all links in the default browser instead of Electron
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url)
+    return { action: 'deny' }
+  })
+}
+
+app.whenReady().then(createWindow)
